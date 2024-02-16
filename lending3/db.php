a18cb6986982be4584dd734bcca6fde355da2ee6
haramborac <82148658+haramborac@users.noreply.github.com>
2024-02-16 11:14:28 +0800

    Delete lending3 directory

--- lending3/db.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    $connection = mysqli_connect('localhost', 'root', '', 'test2');
-    //dbs8473633
-?>
--- lending3/index.php
+++
@@ -1,7 +0,0 @@
-<html>
-<head>
-<meta http-equiv="refresh" content="0.1; URL=https://goyolending.com/login.php" />
-</head>
-<body>
-</body>
-</html>
